0cc8155b71e2ab81ca7c564003bb980bff919b3b
추성훈 <licsh123@naver.com>
2023-01-30 21:02:26 +0900

    #7 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기기술:
    + 스프링 웹
    + 액츄에이터
    + 데브툴즈
    + 롬복

---
+++ server/ShareSpaceProject/src/test/java/com/org/webrepo/sharespaceproject/ShareSpaceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.webrepo.sharespaceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareSpaceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

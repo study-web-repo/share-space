184d6a4cf1a193924d334b4af0f1027008d530d1
추성훈 <licsh123@naver.com>
2023-04-23 23:01:13 +0900

    #10 - 도메인 코드를 엔티티로 변환
    1 도메인 클래스 == 1 테이블 매칭되도록 설계
    생성자와 'equals()', 'hashcode()' 등이
    도메인 맥락에 맞게끔 디테일하게 설정

--- server/ShareSpaceProject/src/main/java/com/org/webrepo/sharespaceproject/domain/Content.java
+++ server/ShareSpaceProject/src/main/java/com/org/webrepo/sharespaceproject/domain/Content.java
@@ -1,18 +1,63 @@
 package com.org.webrepo.sharespaceproject.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "reserveDate"),
+        @Index(columnList = "closedDays")
+})
+@Entity
 public class Content {
-    private long spaceId;
-    private long id;
-    private String content;
-    private int openingHour;
-    private String closedDays;
-    private String notice;
-    private String refundPolicy;
-    private String createdBy;
-    private String facilityInformation;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Setter @Column(nullable = false) private String content;
+    @Setter @Column(nullable = false) private int openingHour;
+    @Setter @Column(nullable = false) private String closedDays;
+    @Setter @Column(nullable = false) private String notice;
+    @Setter @Column(nullable = false) private String refundPolicy;
+    @CreatedBy @Column(nullable = false) private String createdBy;
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
+    @Setter @Column(nullable = false) private String facilityInformation;
+    @Setter @Column(nullable = false) private LocalDateTime reserveDate;
 
-    private LocalDateTime reserveDate;
+    protected Content() {
+    }
 
+    private Content(String content, int openingHour, String closedDays, String notice, String refundPolicy, String facilityInformation, LocalDateTime reserveDate) {
+        this.content = content;
+        this.openingHour = openingHour;
+        this.closedDays = closedDays;
+        this.notice = notice;
+        this.refundPolicy = refundPolicy;
+        this.facilityInformation = facilityInformation;
+        this.reserveDate = reserveDate;
+    }
+
+    public static Content of(String content, int openingHour, String closedDays, String notice, String refundPolicy, String facilityInformation, LocalDateTime reserveDate){
+        return new Content(content, openingHour, closedDays, notice, refundPolicy, facilityInformation, reserveDate);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Content content)) return false;
+        return id != null && Objects.equals(id, content.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
